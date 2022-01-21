03664dad4d51ac0eec1e51db356f84770ad6e1d0
Mike Starr <imhs@protonmail.com>
2022-01-21 06:06:41 -0800

    Follow along complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
 
-document.addEventListener('DOMContentLoaded', init);
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.children[1];
+    console.log(input);
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    inputForm.reset();
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
